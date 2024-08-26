60718ad3c9942d6561a7b27a2fe5c8bd514c309b
LAPTOP-EFS5H6SO\lucil <lucilaoliver8@gmail.com>
2024-08-26 14:59:49 -0300

    Fixed issue with ball movement

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    
+    [SerializeField]
+    private float initialVelocity = 6f;
+
+    private Rigidbody2D ballRb;
+
+
+    void Start()
+    {
+        ballRb = GetComponent<Rigidbody2D>();
+        Launch();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Launch()
+    {
+
+        float xVelocity = Random.Range(0, 2) == 0 ? 1 : -1;
+        float yVelocity = Random.Range(0, 2) == 0 ? 1 : -1;
+        ballRb.velocity = new Vector2(xVelocity, yVelocity) * initialVelocity;
+        Debug.Log(ballRb);
+    }
+}
--- Assets/Scripts/Spawner/SpawnManager.cs
+++ Assets/Scripts/Spawner/SpawnManager.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
@@ -10,24 +8,18 @@ public class SpawnManager : MonoBehaviour
     [SerializeField]
     private float timeToSpawn;
 
-    [SerializeField]
-    private float initialVelocity = 6f;
-
     private float totalTime;
 
     private bool spawned = false;
 
-    private Rigidbody2D ballRb;
-
     private void Start()
     {
         totalTime = 0;
-        ballRb = obstaclePrefab.GetComponent<Rigidbody2D>();
-        Launch();
     }
 
     private void Update()
     {
+        
         totalTime += Time.deltaTime;
 
 
@@ -36,16 +28,8 @@ public class SpawnManager : MonoBehaviour
             Instantiate(obstaclePrefab, new Vector2(0, 0), Quaternion.identity);
             spawned = true;
         }
+        
 
         
-    }
-
-    private void Launch()
-    {
-        
-        float xVelocity = Random.Range(0, 2) == 0 ? 1 : -1;
-        float yVelocity = Random.Range(0, 2) == 0 ? 1 : -1;
-        ballRb.velocity = new Vector2(xVelocity, yVelocity) * initialVelocity;
-        Debug.Log(ballRb);
     }
 }
